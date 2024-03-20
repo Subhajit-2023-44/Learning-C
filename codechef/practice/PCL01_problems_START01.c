687711d6031bfab47482edff308a2d9db7ae8167
Subhajit-2023-44 <subhajit2006patra@gmail.com>
2024-03-20 15:03:00 +0530

    first code

---
+++ codechef/practice/PCL01_problems_START01.c
@@ -0,0 +1,12 @@
+// Number Mirror
+// Write a program that takes a number 
+// N as the input, and prints it to the output.
+
+#include<stdio.h>
+
+int main(){
+    int a;
+    scanf("%d",&a);
+    printf("%d",a);
+    return 0;
+}
