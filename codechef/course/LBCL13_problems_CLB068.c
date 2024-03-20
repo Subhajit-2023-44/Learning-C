33f853cd9508d4629f57d97e5d0cd23cb11f1b26
Subhajit-2023-44 <subhajit2006patra@gmail.com>
2024-03-20 14:11:33 +0530

    twenty-ninth code

--- codechef/course/LBCL13_problems_CLB068.c
+++ codechef/course/LBCL13_problems_CLB068.c
@@ -7,3 +7,29 @@
 // Otherwise, output "Ram & Karan have the same weight!"
 // Your task is to complete the blanks in the code.
 
+#include <stdio.h>
+
+int main() {
+
+    int r = 24;
+    int k = 32;
+    
+    if (r > k) {
+        printf("Ram is heavier than Karan.\n");
+    } else if (r < k) {
+        printf("Karan is heavier than Ram\n");
+    } else {
+        printf("Ram & Karan have the same weight!\n");
+    }
+    
+    r = 78;
+    k = 78;
+    if (r > k) {
+        printf("Ram is heavier than Karan.");
+    } else if (r < k) {
+        printf("Karan is heavier than Ram");
+    } else {
+        printf("Ram & Karan have the same weight!");
+        return 0;
+    }
+    
